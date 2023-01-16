62462a7ab1e082a3476708ba29709549417e2d33
Charan <charanvenkatchandran@gmail.com>
2023-01-16 15:46:41 -0600

    First Commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import styles from './test.module.css'
+
+const cool: string = 'e'
+
+export default function toDoApp() {
+    return (
+        <div className="flex h-96 items-center justify-center">
+            <p>I am vertically and horizontally centered.</p>
+        </div>
+    )
+}
