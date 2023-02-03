9c0354323f6674b0532c87a704d9436ff1bc73f6
Charan <charanvenkatchandran@gmail.com>
2023-02-02 23:27:31 -0600

    r

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,9 @@
-import styles from './test.module.css'
-
-const cool: string = 'e'
-
 export default function toDoApp() {
     return (
-        <div className="flex h-96 items-center justify-center">
-            <p>I am vertically and horizontally centered.</p>
+        <div className="flex h-screen items-center justify-center">
+            <div className="m-auto flex h-1/2 w-[50vw] items-center justify-center bg-slate-900 align-middle">
+                <p>I am vertically and horizontally centered.</p>
+            </div>
         </div>
     )
 }
